7e648d39e6743f32243571c90729bee72aafdab2
sattarvoybek <sattarvoybek>
2018-01-12 19:55:03 +0300

    gs

--- aospgs.mk
+++ aospgs.mk
@@ -19,9 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/NUBIA/NE501J/NE501J.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aospgs/aospgs.mk)
 
-PRODUCT_NAME := lineage_NE501J
+PRODUCT_NAME := aospgs_NE501J
 PRODUCT_DEVICE := NE501J
 PRODUCT_MANUFACTURER := Nubia
 PRODUCT_MODEL := NE501J
